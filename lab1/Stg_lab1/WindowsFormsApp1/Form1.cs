fed42504d472de00ccd71bde37beb3a7ec3ec6d9
BorisZhur <50907618+BorisZhur@users.noreply.github.com>
2020-04-11 16:45:07 +0300

    1

--- lab1/Stg_lab1/WindowsFormsApp1/Form1.cs
+++ lab1/Stg_lab1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,16 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        public static string initStr = "абвгдежзийклмнопрстуфхцчшщъыьэюя.,!?";
+        public static string rusLetters = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+        public static string engLetters = "abcdefghijklmnopqrstuvwxyz";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //StreamReader inp = new StreamReader("" + textBox1.Text + ".txt", Encoding.GetEncoding(1251));
+
         }
     }
 }
