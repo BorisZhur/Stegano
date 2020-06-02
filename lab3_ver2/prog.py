373cfd56902f47410de8ca946d5690a670f3d57d
BorisZhur <50907618+BorisZhur@users.noreply.github.com>
2020-06-02 09:55:52 +0300

    4
    1

--- lab3_ver2/prog.py
+++ lab3_ver2/prog.py
@@ -4,106 +4,95 @@ from cv2 import dct, idct
 from PIL import Image
 import numpy as np
 
-def int_r(num):
-    num = int(num + (0.5 if num > 0 else -0.5))
-    return num
+
+def write(binWrd):
+    image = Image.open("input.bmp") #Открываем изображение.
+    width = image.size[0] #Определяем ширину.
+    height = image.size[1] #Определяем высоту.
+    obj = image.load()
+    
+    imageOutput = Image.open("input1.bmp")
+    obj1 = imageOutput.load()
+    pix = np.array(image.getdata(2))
+    pix = pix.reshape(height,width)
+    binWrdLen = 0
+    for i in range(int(height / 8)):
+        for j in range(int(width / 8)):
+            #print("i ",i)
+            #print("\nj ",j)
+            matrix = np.array(pix[8 * i:8 * (i + 1), 8 * j:8 * (j + 1)])
+            matrix = matrix.astype('float32')
+            matrix_dct = dct(matrix)
+            #print("\nmatrix\n",matrix)
+            #print("\nmatrix_dct\n",matrix_dct)
+            if (binWrdLen < len(binWrd)):
+                for k in range(8):
+                    for l in range(8):
+                        #print(matrix_dct[k][k])
+                        if (binWrdLen < len(binWrd)):
+                            matrix_dct[k][l] = (round(matrix_dct[k][l]))
+                            matrix_dct[k][l] = matrix_dct[k][l] - matrix_dct[k][l] % 2 + int(binWrd[binWrdLen])
+                        binWrdLen += 1
+                #print(idct(matrix_dct))
+                matrix_dct[0][0] = matrix_dct[0][0]/4
+                new_matrix = [round(x) for x in idct(matrix_dct).reshape(1,64)[0]]
+                #print('new matrix', new_matrix)
+                for h in range(0, 64):
+                    rx = h // 8
+                    ry = h % 8
+                    x = list(obj[j * 8 + ry, i * 8 + rx])
+                    x[1] = new_matrix[h]
+                    obj1[j * 8 + ry, i * 8 + rx] = tuple(x)
+    imageOutput.save("output.bmp", "BMP")
+
+def read(wrd,binWrd):
+    binMsg = []
+    imageOutput = Image.open("output.bmp")
+    width = imageOutput.size[0]  # Определяем ширину.
+    height = imageOutput.size[1]  # Определяем высоту.
+    pix1 = np.array(imageOutput.getdata(1))
+    pix1 = pix1.reshape(height,width)
+    binWrdLen = 0
+    for i in range(int(height / 8)):
+        for j in range(int(width / 8)):
+            matrix = np.array(pix1[8 * i:8 * (i + 1), 8 * j:8 * (j + 1)])
+            matrix = matrix.astype('float32')
+            matrix_dct = dct(matrix)
+            if (binWrdLen < len(binWrd)):
+                for k in range(8):
+                    for l in range(8):
+                        if (binWrdLen < len(binWrd)):
+                            matrix_dct[k][l] = (round(matrix_dct[k][l]))
+                            binMsg.append(int(matrix_dct[k][l] % 2))
+                        binWrdLen += 1
 
 
-insertion = str(input("Введите слово\n"))
-insertion2 = ''
-for code in insertion.encode('cp1251'):
+    answer = ''
+    for i in range(len(wrd)):
+        bin_str = ''.join(str(x) for x in (binMsg[(i * 8):(i + 1) * 8]))
+        if bin_str == '10011000':
+            answer += ' '
+        else:
+            answer += str(bytes([int(bin_str, base=2)]), 'cp1251')
+    print("прочитанно - " , answer)
+    check = ''.join(str(x) for x in (binMsg))
+
+    number_of_mistakes = 0
+    for l in range(len(binWrd)):
+        if binWrd[l] != check[l]:
+            number_of_mistakes += 1
+    print('При передаче искажено %d бит' % number_of_mistakes)
+    print('или %f процентов ' % (100.*(float(number_of_mistakes)/ len(binWrd))))
+
+
+
+
+msg = str(input("Введите слово\n"))
+binWrd = ''
+for code in msg.encode('cp1251'):
     a = bin(code)[2:]
     while len(a) < 8:
         a = '0' + a
-    insertion2 += a
-#print('начальное', insertion2)
-
-
-image = Image.open("input.bmp") #Открываем изображение.
-width = image.size[0] #Определяем ширину.
-height = image.size[1] #Определяем высоту.
-obj = image.load()
-
-image1 = Image.open("input1.bmp")
-obj1 = image1.load()
-pix = np.array(image.getdata(1))
-pix = pix.reshape(height,width)
-len_ins = 0
-for i in range(int(height / 8)):
-    for j in range(int(width / 8)):
-        print("i ",i)
-        print("\nj ",j)
-        matrix = np.array(pix[8 * i:8 * (i + 1), 8 * j:8 * (j + 1)])
-        matrix = matrix.astype('float32')
-        dct_coef = dct(matrix)
-        print("\nmatrix\n",matrix)
-        print("\ndct_coef\n",dct_coef)
-        if (len_ins < len(insertion2)):
-            for k in range(8):
-                for l in range(8):
-                    #print(dct_coef[k][k])
-                    if (len_ins < len(insertion2)):
-                        dct_coef[k][l] = (round(dct_coef[k][l]))
-                        if (dct_coef[k][l] % 2 != int(insertion2[len_ins])):
-                            # print(dct_coef[k][k])
-                            # print(insertion2[len_ins])
-                            dct_coef[k][l] -= 1
-                        else:
-                            print('оставляем как есть', dct_coef[k][k], insertion2[len_ins])
-                    len_ins += 1
-            print(idct(dct_coef))
-            dct_coef[0][0] = dct_coef[0][0]/4
-            new_matrix = [round(x) for x in idct(dct_coef).reshape(1,64)[0]]
-            print('new matrix', new_matrix)
-            for h in range(0, 64):
-                rel_x = h // 8
-                rel_y = h % 8
-                x = list(obj[j * 8 + rel_y, i * 8 + rel_x])
-                x[1] = new_matrix[h]
-                obj1[j * 8 + rel_y, i * 8 + rel_x] = tuple(x)
-
-image1.save("output.bmp", "BMP")
-
-message_bin = []
-#Cчитывание сообщения
-pix1 = np.array(image1.getdata(1))
-pix1 = pix1.reshape(height,width)
-len_ins = 0
-for i in range(int(height / 8)):
-    for j in range(int(width / 8)):
-        matrix1 = np.array(pix1[8 * i:8 * (i + 1), 8 * j:8 * (j + 1)])
-        #print(matrix)
-        matrix1 = matrix1.astype('float32')
-        dct_coef = dct(matrix1)
-        if (len_ins < len(insertion2)):
-            for k in range(8):
-                for l in range(8):
-                    if (len_ins < len(insertion2)):
-                        dct_coef[k][l] = (round(dct_coef[k][l]))
-                        if (dct_coef[k][l] % 2 == 1):
-                            message_bin.append(1)
-                        else:
-                            message_bin.append(0)
-                    len_ins += 1
-
-answer = ''
-
-for i in range(len(insertion)):
-    bin_str = ''.join(str(x) for x in (message_bin[(i * 8):(i + 1) * 8]))
-    if bin_str == '10011000':
-        answer += ' '
-    else:
-        answer += str(bytes([int(bin_str, base=2)]), 'cp1251')
-print("Ваше сообщение - " , answer)
-
-check = ''.join(str(x) for x in (message_bin))
-
-#подсчет кол-ва ошибок
-number_of_mistakes = 0
-for l in range(len(insertion2)):
-    if insertion2[l] != check[l]:
-        number_of_mistakes += 1
-print('При передаче допущено %d ошибок' % number_of_mistakes)
-
-
-print('Ошибочно передано %f процентов сообщения ' % (100.*(float(number_of_mistakes)/ len(insertion2))))
+    binWrd += a
+write(binWrd)
+read(msg,binWrd)
